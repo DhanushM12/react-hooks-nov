1ed9b3017669f5e2f274fb451b03d2aab5ab7ed4
DhanushM12 <dhanushmukhi@gmail.com>
2022-09-23 20:25:49 +0530

    Added 2 hooks

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import UseEffect from './hooks/UseEffect';
+import UseState from './hooks/UseState';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <UseState />
+      <br/><br/>
+      <UseEffect />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/UseEffect.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+
+function UseEffect() {
+    const [resource, setResource] = useState('');
+    const [items, setItems] = useState([]);
+    
+    useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+      .then(res => res.json())
+      .then(json => setItems(json))
+    }, [resource])
+    
+  return (
+    <div>
+        <div>
+            <button onClick={() => setResource('posts')}>Posts</button>
+            <button onClick={() => setResource('todos')}>Todos</button>
+            <button onClick={() => setResource('users')}>Users</button>
+        </div>
+        <div>
+            {
+            items.map(item => {
+                return <pre>{JSON.stringify(item)}</pre>
+            })
+}
+        </div>
+    </div>
+  )
+}
+
+export default UseEffect
